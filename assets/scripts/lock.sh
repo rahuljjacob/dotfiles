fa58be1bafcd7dfe5b7f1a42840d1c0ed23e1d9f
Rahul <rahul.j.jacob@gmail.com>
2024-02-28 16:54:47 +0530

    Added Hyprlock

--- assets/scripts/lock.sh
+++ assets/scripts/lock.sh
@@ -2,7 +2,7 @@
 
 if [ -f "/usr/bin/swayidle" ] ; then
 	echo "Swayidle installed"
-	swayidle -w timeout 300 'swaylock -f' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 600 'systemctl suspend'
+	swayidle -w timeout 300 'hyprlock ' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 600 'systemctl suspend'
 else
 	echo "Swayidle not installed"
 fi;
