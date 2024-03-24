470a794fe933a990d6b85fe9190d7e45019294f9
Rahul <rahul.j.jacob@gmail.com>
2024-03-24 16:57:36 +0530

    Switched to hypridle

--- assets/scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-
-if [ -f "/usr/bin/swayidle" ] ; then
-	echo "Swayidle installed"
-	swayidle -w timeout 300 'hyprlock' timeout 360 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' timeout 600 'systemctl suspend'
-else
-	echo "Swayidle not installed"
-fi;
